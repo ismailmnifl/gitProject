7529317775a40bd6d615500e49bf4ff0e2df4c73
chaimaa nmiyesse <chaimaanamisse@gmail.com>
2021-01-19 10:19:16 +0100

    test

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 // la fonction menu  qui permet a l'utilisateur de choisir 
 int menu()
 {			printf("=============================================\n");
+            //printf("votre calculatrice scientifique");
 	        printf("MENU\n");
 	        printf("=============================================\n");
 		    printf("1. les simple operation : + | - | x | / \n2. table de mutiplication d'un nombre\n3. autre operation \n4. finir\n");
@@ -62,6 +63,7 @@ int main()
 							printf("le resultat : %.2f\n",res);
 
 							break;
+					
 							
 						default:
                				 printf(" selection n'exist pas\n");
